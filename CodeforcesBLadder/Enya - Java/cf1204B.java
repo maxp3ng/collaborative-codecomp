6c21eae9920907e0e6fa90d4deb629a0b7da1d83
Enya Chen <127362535+3nya@users.noreply.github.com>
2023-11-05 00:31:23 -0400

    Added documentation

--- CodeforcesBLadder/Enya - Java/cf1204B.java
+++ CodeforcesBLadder/Enya - Java/cf1204B.java
@@ -8,28 +8,34 @@ public class cf1204B {
         int min = scan.nextInt();
         int max = scan.nextInt();
 
+        // Calculating minimum sum first
         int prevNum = 1;
         int minSum = 1;
+
+        // Loop to find the highest number within minimum amt of different numbers
         for (int i = 1; i < min; i++) {
+            // increment by multiplying by 2
             minSum += prevNum * 2;
             prevNum *= 2;
         }
+
+        // After getting the highest possible number, assume the rest of array are 1s
         minSum += n - min;
 
 
+        // Calculating maximum sum (pretty much same process as minimum sum)
         int maxSum = 1;
-        int highestNum = 0;
+        int highestNum = 1;
         prevNum = 1;
         for (int i = 1; i < max; i++) {
             maxSum += prevNum * 2;
             highestNum = prevNum * 2;
             prevNum *= 2;
         }
-        if (highestNum == 0) {
-            maxSum += n - max;
-        } else {
-            maxSum += highestNum * (n - max);
-        }
+
+        // After getting maximum number, assume the rest of array is the maximum number
+        maxSum += highestNum * (n - max);
+
 
 
         System.out.println(minSum + " " + maxSum);
