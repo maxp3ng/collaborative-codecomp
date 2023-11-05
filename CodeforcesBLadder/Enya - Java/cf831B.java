f3e4307283a2d312d5a513c145c573fed18394d4
Enya Chen <127362535+3nya@users.noreply.github.com>
2023-11-04 23:19:53 -0400

    Added documentation
    Easiest question out of entire problemset, start from here

--- CodeforcesBLadder/Enya - Java/cf831B.java
+++ CodeforcesBLadder/Enya - Java/cf831B.java
@@ -12,12 +12,16 @@ public class cf831B {
         StringBuilder out = new StringBuilder();
 
         for (int i = 0; i < intent.length(); i++) {
+            // All if-statements are comparing char to their ASCII value
+
+            // Checks if char is a number and directly adds to output if so
             if (intent.charAt(i) < 65) {
                 out.append(intent.charAt(i));
             } else {
                 boolean up = false;
                 String character = String.valueOf(intent.charAt(i));
 
+                // if char is uppercase temporarily set to lowercase and use boolean value to remember it was once uppercase
                 if (intent.charAt(i) < 97) {
                     String input = String.valueOf(intent.charAt(i));
                     up = true;
@@ -25,6 +29,9 @@ public class cf831B {
 
                 }
 
+                // loop through first layout to find the given char
+                // then use its index to locate the corresponding char on the second layout
+                // add to output
                 for (int j = 0; j < str1.length(); j++) {
                     if (character.equals(String.valueOf(str1.charAt(j)))) {
                         if (up) {
