0e904b2531d394aa17ca43730e8dadb99a574cc5
singularity <maxpeng48@gmail.com>
2023-11-05 19:13:14 -0500

    Create cf805B_3Palindrome.cpp

---
+++ CodeforcesBLadder/Max - C++/cf805B_3Palindrome.cpp
@@ -0,0 +1,53 @@
+// https://codeforces.com/problemset/problem/805/B
+// solved 11/5/23
+// comments: unbelievably easy once you get it -> think problems through!
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const ll MXN = 2e5 + 10;
+using str = string;
+using db = long double;
+using vll = vector<ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define endl "\n";
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+
+#define Rep(i, x, y)   for (__typeof(x) i=x; i < y; i++)
+#define Repi(i, x, y)  for (__typeof(x) i=x; i > y; i--)
+#define vOut(v) Rep(i,0,v.size()){cout << v[i] << " ";} cout << endl
+#define Out(s)  cout << (s) << '\n'
+#define OutRet(s)  cout << (s) << '\n'; return
+#define ts(n) to_string(n)
+
+void solve(){
+    ll n, m;
+    cin >> n;
+    str s = "";
+
+    for (int i=0; i<n; i++){
+        if ((i/2)%2==0){
+           s += 'a';
+        } else {
+            s += 'b';
+        }
+    }
+    Out(s);
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+//    ll T = 1; cin >> T;
+//    while(T --){
+        solve();
+//    }
+
+    return 0;
+}
