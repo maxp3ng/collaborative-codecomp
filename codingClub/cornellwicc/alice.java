fd60645b07f95ac843c3d1f3de083db66b419534
Enya Chen <127362535+3nya@users.noreply.github.com>
2024-03-05 18:33:52 -0500

    Add files via upload

---
+++ codingClub/cornellwicc/alice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class alice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x1 = scan.nextInt();
+        int x2 = scan.nextInt();
+        int y1 = scan.nextInt();
+        int y2 = scan.nextInt();
+
+
+    }
+}
